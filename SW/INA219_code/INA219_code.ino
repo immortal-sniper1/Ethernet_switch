d330453f7fe970d0eae442f295d67c3e98a9df0f
immortal_sniper1 <robikecs1234@gmail.com>
2024-07-27 23:07:22 +0300

    t

--- SW/INA219_code/INA219_code.ino
+++ SW/INA219_code/INA219_code.ino
@@ -8,16 +8,72 @@ uint16_t my_shunt= 50; // mohm
 void setup()
 {
   Serial.begin(115200);
+
+
+Wire.begin(2,1);
+
+
+
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+
+
+
+
   Serial.println(__FILE__);
   Serial.print("INA219_LIB_VERSION: ");
   Serial.println(INA219_LIB_VERSION);
 
-  Wire.begin();
   if (!INA.begin() )
   {
     Serial.println("could not connect. Fix and Reboot");
   }
  // INA.setBusVoltageRange(16);
+
+  INA.setMaxCurrentShunt(5, 0.05);
+  INA.setGain(1);
+  INA.setShuntResolution(12);
+  INA.setBusResolution(12);
+  INA.setShuntSamples(5);
+
+
+
+
 
  pinMode(36, OUTPUT);
  digitalWrite(36, 1);
@@ -32,15 +88,34 @@ void setup()
 void loop() 
 {
   //  these two can be read without further configuration.
+  Serial.print("Bus voltage:  ");
   Serial.print(INA.getBusVoltage(), 3);
   Serial.print("\t");
   Serial.print(INA.getShuntVoltage_mV(), 3);
   Serial.println();
-  Serial.print("Load current: ");
-  Serial.print( INA.getShuntVoltage_mV() * my_shunt /1000);
-  Serial.println(" mA ");
+  Serial.print("Load current in mA: ");
+  Serial.print( INA.getShuntVoltage_mV() * my_shunt /1000 , 3 );
+  Serial.println();
   delay(10000);
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
